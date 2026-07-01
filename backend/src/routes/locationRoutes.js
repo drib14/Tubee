84d5459f0e7a6490680f5da8f2034512b0956c76
drib14 <jhondribramirez7@gmail.com>
2026-07-02 02:18:42 +0800

    refactor: remove unused interface definitions and deprecated utility methods

--- backend/src/routes/locationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { reverseGeocode } from '../controllers/locationController.js';
-
-const router = express.Router();
-
-router.get('/reverse', reverseGeocode);
-
-export default router;
